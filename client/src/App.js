3f0fbf20450b11c0db3b0faeb7861ba67b218fe8
Roshan R <72537120+learner1204@users.noreply.github.com>
2023-07-04 20:12:49 +0000

    work in progress

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+  <main>
+    <header>
+      <a href="" classname="logo">MyBlog</a>
+      <nav>
+        <a href="">Logic</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+  </main>
+  );
+}
+
+export default App;
